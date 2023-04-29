e6884511ebfa83e0e174cec98cc7345e64bbedc9
gimgyuyeon <8080claire@naver.com>
2023-04-29 21:02:40 +0900

    🎉 init : init project

---
+++ chatserver/websocket/src/main/java/org/example/model/Message.java
@@ -0,0 +1,18 @@
+package org.example.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+
+}
